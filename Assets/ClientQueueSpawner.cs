3de8dc19d10130866340bc5f4d10eeeb6a5461a1
astridt3 <rocio.beck05@gmail.com>
2026-08-24 13:30:23 -0300

    fila pulida

--- Assets/ClientQueueSpawner.cs
+++ Assets/ClientQueueSpawner.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
 
 public class ClientQueueSpawner : MonoBehaviour
@@ -9,33 +10,47 @@ public class ClientQueueSpawner : MonoBehaviour
     [Header("Queue Positions")]
     [SerializeField] private Transform[] queuePositions;
 
+    [Header("Spawn Settings")]
+    [SerializeField] private int maxClients = 3;
+    [SerializeField] private float spawnInterval = 5f;
+
     private List<RestaurantClient> clients = new List<RestaurantClient>();
 
     private void Start()
     {
-        SpawnClients();
+        StartCoroutine(SpawnClientsCoroutine());
     }
 
-    private void SpawnClients()
+    private IEnumerator SpawnClientsCoroutine()
     {
-        int amountToSpawn = Mathf.Min(3, queuePositions.Length);
+        int amountToSpawn = Mathf.Min(maxClients, queuePositions.Length);
 
         for (int i = 0; i < amountToSpawn; i++)
         {
-            GameObject clientObject = Instantiate(
-                clientPrefab,
-                queuePositions[i].position,
-                queuePositions[i].rotation
-            );
+            SpawnClient(i);
 
-            RestaurantClient client = clientObject.GetComponent<RestaurantClient>();
-
-            if (client != null)
+            // No esperamos después del último cliente
+            if (i < amountToSpawn - 1)
             {
-                clients.Add(client);
-
-                client.Setup(this, i);
+                yield return new WaitForSeconds(spawnInterval);
             }
+        }
+    }
+
+    private void SpawnClient(int queueIndex)
+    {
+        GameObject clientObject = Instantiate(
+            clientPrefab,
+            queuePositions[queueIndex].position,
+            queuePositions[queueIndex].rotation
+        );
+
+        RestaurantClient client = clientObject.GetComponent<RestaurantClient>();
+
+        if (client != null)
+        {
+            clients.Add(client);
+            client.Setup(this, queueIndex);
         }
 
         UpdateQueue();
