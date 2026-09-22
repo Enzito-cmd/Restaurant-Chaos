dca1ca1f79ba37d654f36c734311dd161bb648af
astridt3 <rocio.beck05@gmail.com>
2026-09-22 15:39:39 -0300

    arreglo del material de cliente

--- Assets/Scripts/ClientHighlight.cs
+++ Assets/Scripts/ClientHighlight.cs
@@ -1,114 +1,74 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ClientHighlight : MonoBehaviour
 {
-    [Header("Highlight")]
-    [SerializeField] private Color emissionColor = Color.yellow;
-
-    [SerializeField, Range(0f, 10f)]
-    private float emissionIntensity = 1.5f;
+    [SerializeField] private Color highlightColor = Color.yellow;
+    [SerializeField] private float brightness = 2f;
 
     private Renderer[] renderers;
-
-    private readonly List<Material> materials = new List<Material>();
-
+    private Color[] originalColors;
 
     private void Awake()
     {
-        renderers = GetComponentsInChildren<Renderer>(true);
+        renderers = GetComponentsInChildren<Renderer>();
 
-        PrepareMaterials();
+        int cantidadMateriales = 0;
 
-        SetHighlight(false);
-    }
-
-
-    private void PrepareMaterials()
-    {
-        materials.Clear();
-
-        foreach (Renderer renderer in renderers)
+        for (int i = 0; i < renderers.Length; i++)
         {
-            if (renderer == null)
-                continue;
-            Material[] rendererMaterials = renderer.materials;
+            cantidadMateriales += renderers[i].materials.Length;
+        }
 
-            foreach (Material material in rendererMaterials)
+        originalColors = new Color[cantidadMateriales];
+
+        int index = 0;
+
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            Material[] materials = renderers[i].materials;
+
+            for (int j = 0; j < materials.Length; j++)
             {
-                if (material == null)
-                    continue;
+                if (materials[j].HasProperty("_BaseColor"))
+                {
+                    originalColors[index] =
+                        materials[j].GetColor("_BaseColor");
+                }
 
-                materials.Add(material);
-
-                PrepareEmission(material);
+                index++;
             }
         }
     }
 
-
-    private void PrepareEmission(Material material)
-    {
-        Texture baseTexture = null;
-
-        if (material.HasProperty("_BaseMap"))
-        {
-            baseTexture = material.GetTexture("_BaseMap");
-        }
-        else if (material.HasProperty("_MainTex"))
-        {
-            baseTexture = material.GetTexture("_MainTex");
-        }
-
-        if (baseTexture != null &&
-            material.HasProperty("_EmissionMap"))
-        {
-            material.SetTexture(
-                "_EmissionMap",
-                baseTexture
-            );
-        }
-    }
-
-
     public void SetHighlight(bool active)
     {
-        foreach (Material material in materials)
+        int index = 0;
+
+        for (int i = 0; i < renderers.Length; i++)
         {
-            if (material == null)
-                continue;
+            Material[] materials = renderers[i].materials;
 
-            SetEmission(material, active);
-        }
-    }
+            for (int j = 0; j < materials.Length; j++)
+            {
+                if (materials[j].HasProperty("_BaseColor"))
+                {
+                    if (active)
+                    {
+                        Color color = highlightColor * brightness;
 
+                        materials[j].SetColor("_BaseColor", color);
+                    }
+                    else
+                    {
+                        materials[j].SetColor(
+                            "_BaseColor",
+                            originalColors[index]
+                        );
+                    }
+                }
 
-    private void SetEmission(Material material, bool active)
-    {
-        if (!material.HasProperty("_EmissionColor"))
-            return;
-
-
-        if (active)
-        {
-            material.EnableKeyword("_EMISSION");
-
-            Color finalColor =
-                emissionColor * emissionIntensity;
-
-            material.SetColor(
-                "_EmissionColor",
-                finalColor
-            );
-        }
-        else
-        {
-            material.SetColor(
-                "_EmissionColor",
-                Color.black
-            );
-
-            material.DisableKeyword("_EMISSION");
+                index++;
+            }
         }
     }
 }
