963e0da468afea723e3082a9df2206ec7bab6766
astridt3 <rocio.beck05@gmail.com>
2026-09-22 11:25:10 -0300

    mejora shader cliente respetando colores

--- Assets/Scripts/ClientHighlight.cs
+++ Assets/Scripts/ClientHighlight.cs
@@ -1,53 +1,104 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ClientHighlight : MonoBehaviour
 {
     [Header("Highlight")]
     [SerializeField] private Color emissionColor = Color.yellow;
-    [SerializeField] private float emissionIntensity = 2f;
+
+    [SerializeField, Range(0f, 10f)]
+    private float emissionIntensity = 1.5f;
 
     private Renderer[] renderers;
-    private Material[] materials;
+
+    private readonly List<Material> materials = new List<Material>();
+
 
     private void Awake()
     {
-        // Incluye MeshRenderer y SkinnedMeshRenderer
-        renderers = GetComponentsInChildren<Renderer>();
+        renderers = GetComponentsInChildren<Renderer>(true);
 
-        materials = new Material[renderers.Length];
+        PrepareMaterials();
 
-        for (int i = 0; i < renderers.Length; i++)
+        SetHighlight(false);
+    }
+
+
+    private void PrepareMaterials()
+    {
+        materials.Clear();
+
+        foreach (Renderer renderer in renderers)
         {
-            materials[i] = renderers[i].material;
+            if (renderer == null)
+                continue;
+            Material[] rendererMaterials = renderer.materials;
 
-            // Apagado inicialmente
-            SetEmission(materials[i], false);
+            foreach (Material material in rendererMaterials)
+            {
+                if (material == null)
+                    continue;
+
+                materials.Add(material);
+
+                PrepareEmission(material);
+            }
         }
     }
+
+
+    private void PrepareEmission(Material material)
+    {
+        Texture baseTexture = null;
+
+        if (material.HasProperty("_BaseMap"))
+        {
+            baseTexture = material.GetTexture("_BaseMap");
+        }
+        else if (material.HasProperty("_MainTex"))
+        {
+            baseTexture = material.GetTexture("_MainTex");
+        }
+
+        if (baseTexture != null &&
+            material.HasProperty("_EmissionMap"))
+        {
+            material.SetTexture(
+                "_EmissionMap",
+                baseTexture
+            );
+        }
+    }
+
 
     public void SetHighlight(bool active)
     {
-        for (int i = 0; i < materials.Length; i++)
+        foreach (Material material in materials)
         {
-            if (materials[i] == null)
+            if (material == null)
                 continue;
 
-            SetEmission(materials[i], active);
+            SetEmission(material, active);
         }
     }
+
 
     private void SetEmission(Material material, bool active)
     {
         if (!material.HasProperty("_EmissionColor"))
             return;
 
+
         if (active)
         {
             material.EnableKeyword("_EMISSION");
 
+            Color finalColor =
+                emissionColor * emissionIntensity;
+
             material.SetColor(
                 "_EmissionColor",
-                emissionColor * emissionIntensity
+                finalColor
             );
         }
         else
